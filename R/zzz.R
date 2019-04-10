f23392e49cbc83fc68f72dae2d7caf7810b0b235
Patrick Kimes <patrick.kimes@gmail.com>
2019-04-09 22:13:13 -0400

    Fix typo in zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 #' @import ExperimentHub
 #' @importFrom utils read.csv
 .onLoad <- function(libname, pkgname) {
-  fl <- system.file("extdata", "metadata.csv", package="bodymapRat")
+  fl <- system.file("extdata", "metadata.csv", package="benchmarkfdrData2019")
   titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
   createHubAccessors(pkgname, titles)
 }
