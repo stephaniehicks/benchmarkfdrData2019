0b8c964539ba6ad0e1836fbbd0390ac2717d82bb
Stephanie Hicks <stephaniechicks@gmail.com>
2025-03-04 20:50:51 -0500

    starting Bioc deprecation process

--- R/zzz.R
+++ R/zzz.R
@@ -5,3 +5,10 @@
   titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
   createHubAccessors(pkgname, titles)
 }
+
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.22")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
